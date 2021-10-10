1bc0b99873017f34e2cdd9a44540229dbba6461a
Gulvan <mitmitmitmit@yandex.ru>
2021-10-10 17:02:17 +0300

    Versioning build script

---
+++ build.bat
@@ -0,0 +1,6 @@
+del .\Export\html5\bin\*.js
+FOR /F "tokens=* USEBACKQ" %%F IN (`powershell -command "[int32](New-TimeSpan -Start (Get-Date "01/01/1970") -End (Get-Date)).TotalSeconds"`) DO (
+SET buildtime=%%F
+)
+powershell -Command "(gc project.xml) -replace 'file=\"\d*?\"', 'file=\"%buildtime%\"' | Out-File -encoding ASCII project.xml"
+lime build html5
