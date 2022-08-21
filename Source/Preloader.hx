b2a8dd5cd7363bcf19486c3b0990df6754f9931d
Gulvan <mitmitmitmit@yandex.ru>
2022-08-22 01:28:57 +0300

    Preloader

---
+++ Source/Preloader.hx
@@ -0,0 +1,22 @@
+package;
+
+import openfl.Lib;
+import openfl.Assets;
+import openfl.display.Preloader.DefaultPreloader;
+
+class Preloader extends DefaultPreloader
+{
+ 	public override function onInit():Void 
+    {
+        Assets.loadLibrary("preloader").onComplete (function (_) {
+            
+            trace("preloader loaded! ");
+            
+            var mc = Assets.getMovieClip("preloader:LogoPreloader");
+            mc.x = (Lib.current.stage.stageWidth  )/ 2;
+            mc.y = (Lib.current.stage.stageHeight ) / 2;
+            addChildAt (mc,0);
+        });
+        
+    }
+}
