676d36e2c066989551ca7def3718870aa7f32b17
Gulvan <mitmitmitmit@yandex.ru>
2023-02-27 19:28:20 +0300

    Gallery component

---
+++ src/gfx/game/Gallery.hx
@@ -0,0 +1,41 @@
+package gfx.game;
+
+import haxe.ui.core.Component;
+import haxe.ui.events.MouseEvent;
+import haxe.ui.containers.HBox;
+
+@:xml('
+    <hbox>
+        <style>
+            .galleryBtn {
+                pointer-events: true;
+            }
+            .galleryBtn:hover {
+                background-color: 0xEEEEEE;
+                background-opacity: 0.5;
+            }
+        </style>
+        <image id="leftBtn" resource="assets/symbols/live/gallery_left.svg" verticalAlign="center" width="10%" height="80%" styleName="galleryBtn" />
+        <stack id="contentStack" width="80%" height="100%" />
+        <image id="rightBtn" resource="assets/symbols/live/gallery_right.svg" verticalAlign="center" width="10%" height="80%" styleName="galleryBtn" />
+    </hbox>
+')
+class Gallery extends HBox
+{
+    @:bind(leftBtn, MouseEvent.CLICK)
+    private function onLeftPressed(e) 
+    {
+        contentStack.prevPage();
+    }
+
+    @:bind(rightBtn, MouseEvent.CLICK)
+    private function onRightPressed(e) 
+    {
+        contentStack.nextPage();
+    }
+
+    public function addPage(comp:Component)
+    {
+        contentStack.addComponent(comp);
+    }
+}
