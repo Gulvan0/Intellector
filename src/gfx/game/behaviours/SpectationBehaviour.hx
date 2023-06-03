6521795db4a725ea286cff64243b68fb2b853ba6
Gulvan <mitmitmitmit@yandex.ru>
2023-06-04 00:03:43 +0300

    SpectationBehaviour

---
+++ src/gfx/game/behaviours/SpectationBehaviour.hx
@@ -0,0 +1,71 @@
+package gfx.game.behaviours;
+
+import net.shared.dataobj.OfferAction;
+import net.shared.dataobj.OfferKind;
+import net.shared.PieceColor;
+import gfx.game.events.GameboardEvent;
+import GlobalBroadcaster.GlobalEvent;
+import net.shared.dataobj.TimeReservesData;
+import gfx.game.models.SpectationModel;
+
+class SpectationBehaviour extends GameRelatedBehaviour
+{
+    private var spectationModel:SpectationModel;
+
+    private function setPlayerOnlineStatus(playerColor:PieceColor, online:Bool)
+    {
+        spectationModel.playerOnline.set(playerColor, online);
+    }
+
+    private function updateOfferStateDueToAction(offerSentBy:PieceColor, offer:OfferKind, action:OfferAction)
+    {
+        var active:Bool = action.match(Create);
+        spectationModel.outgoingOfferActive[offerSentBy].set(offer, active);
+    }
+
+    private function onOfferActionRequested(kind:OfferKind, action:OfferAction)
+    {
+        trace('Unexpected offer action (${action}/${kind}) for SpectatorBehaviour');
+    }
+
+    public function handleGlobalEvent(event:GlobalEvent)
+    {
+        //* Do nothing
+    }
+
+    public function handleGameboardEvent(event:GameboardEvent)
+    {
+        //* Do nothing
+    }
+
+    private function customOnEntered()
+    {
+        //* Do nothing
+    }
+
+    private function onInvalidMove()
+    {
+        trace("Unexpected InvalidMove for SpectatorBehaviour");
+    }
+
+    private function onMoveAccepted(timeData:Null<TimeReservesData>)
+    {
+        trace("Unexpected MoveAccepted for SpectatorBehaviour");
+    }
+
+    private function updateBehaviourDueToTurnColorUpdate()
+    {
+        //* Do nothing
+    }
+
+    private function isAutoscrollEnabled():Bool
+    {
+        return Preferences.autoScrollOnMove.get().match(Always);
+    }
+
+    public function new(spectationModel:SpectationModel)
+    {
+        super(spectationModel);
+        this.spectationModel = spectationModel;
+    }
+}
