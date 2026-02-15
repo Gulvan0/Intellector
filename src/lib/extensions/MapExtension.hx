d4a82f1093d57cbef9fba4aac2c913af8b41864b
Gulvan <mitmitmitmit@yandex.ru>
2026-02-15 21:11:52 +0300

    Basic libs

---
+++ src/lib/extensions/MapExtension.hx
@@ -0,0 +1,13 @@
+package lib.extensions;
+
+class MapExtension
+{
+    public static function mergeWith<K, V>(map1: Map<K, V>, map2: Map<K, V>, prioritizeOriginal: Bool = false): Map<K, V>
+    {
+        var result = map1.copy();
+        for (key => value in map2)
+            if (!prioritizeOriginal || !result.exists(key))
+                result.set(key, value);
+        return result;
+    }
+}
