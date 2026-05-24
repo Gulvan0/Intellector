378bf928c8973ffe1d085d5ab13764e126173d2d
Gulvan <mitmitmitmit@yandex.ru>
2026-05-24 14:28:23 +0200

    Added missing base class fields to challenge creation payload models

--- src/net/models/challenge/ChallengeCreateDirect.hx
+++ src/net/models/challenge/ChallengeCreateDirect.hx
@@ -6,6 +6,9 @@ import lib.json.IJsonSerializableMacro;
 @:structInit
 class ChallengeCreateDirect implements IJsonSerializableMacro
 {
+	public var rated:Bool;
+	public var acceptor_color:ChallengeAcceptorColor;
+	@:default(null) public var custom_starting_sip:Null<String>;
 	public var fischer_time_control:Null<TimeControl> = null;
 	public var callee_ref:String;
 }
--- src/net/models/challenge/ChallengeCreateOpen.hx
+++ src/net/models/challenge/ChallengeCreateOpen.hx
@@ -6,6 +6,9 @@ import lib.json.IJsonSerializableMacro;
 @:structInit
 class ChallengeCreateOpen implements IJsonSerializableMacro
 {
+	public var rated:Bool;
+	public var acceptor_color:ChallengeAcceptorColor;
+	@:default(null) public var custom_starting_sip:Null<String>;
 	public var fischer_time_control:Null<TimeControl> = null;
 	public var link_only:Bool;
 }
